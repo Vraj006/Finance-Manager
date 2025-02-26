4073dee2ead3a9dcde3d9f45e0e70a5915e4989e
Vraj Prajapati <vrajprajapati28506@gmail.com>
2025-02-27 01:10:22 +0530

    Update Webpack config to handle environment variables

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,38 +1,53 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
+module.exports = (env) => {
+  // Load environment variables from .env file
+  const envVars = dotenv.config().parsed;
+
+  // Convert environment variables to a format Webpack can use
+  const envKeys = Object.keys(envVars).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(envVars[next]);
+    return prev;
+  }, {});
+
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+      publicPath: '/'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
         }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html'
-    })
-  ],
-  devServer: {
-    historyApiFallback: true,
-    port: 3000
-  }
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html'
+      }),
+      // Add the DefinePlugin to inject environment variables
+      new webpack.DefinePlugin(envKeys)
+    ],
+    devServer: {
+      historyApiFallback: true,
+      port: 3000
+    }
+  };
 };
